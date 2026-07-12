c3e76e036a9cac25fa25785daab95aa8b925d203
mmi2804-crypto <mmi2804@gmail.com>
2026-07-13 03:03:58 +0400

    Add files via upload
    ….2

---
+++ sw.js
@@ -0,0 +1,21 @@
+var CACHE = 'sentences-v2';
+var FILES = ['./index.html', './manifest.json'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(r){
+      var cl=r.clone();
+      caches.open(CACHE).then(function(c){c.put(e.request,cl);});
+      return r;
+    }).catch(function(){ return caches.match(e.request); })
+  );
+});
